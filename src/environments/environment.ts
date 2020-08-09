7851e07438f3baa4e3c2c21e6f2b107682082ffe
cubahorizontes <66374464+cubahorizontes@users.noreply.github.com>
2020-08-09 14:23:06 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   stripeKey: 'pk_live_WyOFBm9pnkQiT6XNT9jrQjUp00reXHLLzf',
   //stripeKey: 'pk_test_OWmj8OkGjUqxLyrgrbHtXR7600FZYXhUa3',
-  //apiURL: 'https://appvirtualrg.com/',
-  apiURL: 'https://cocinalaabuela.com/'
+  apiURL: 'https://appvirtualrg.com/',
+  //apiURL: 'https://cocinalaabuela.com/'
   //apiURL: 'http://ringtelbackend.local/'
 };
 
